452644684817e2bcc7f080e39533cc28e9c0ee23
tinaxd <31384595+tinaxd@users.noreply.github.com>
2021-05-23 11:42:22 +0900

    tcp

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("foo");
+import net = require('net');
+
+const server = net.createServer((conn) => {
+    console.log('server listening on port 15243');
+
+    conn.on('data', (data) => {
+        console.log(data);
+        conn.write('hello');
+    });
+
+    conn.on('close', () => {
+        console.log('server closed');
+    })
+});
+server.listen(15243);
